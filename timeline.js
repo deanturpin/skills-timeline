68eac5ac24f4a4ba24e0ae6e7c8c7a80a52b6160
deanturpin <git@lvbtn.uk>
2016-12-23 08:23:49 +0000

    add some skills and dates

--- timeline.js
+++ timeline.js
@@ -1,3 +1,17 @@
 #!/usr/bin/node
 
-console.log("timeline")
+"use strict"
+
+const today = Date()
+console.log("today", today)
+
+var skills = [
+	["C++", Date("1998"), Date("2016")],
+	["Linux", Date("2006"), Date("2016")]
+]
+
+console.log("total skills", skills.length)
+
+for (var i = 0; i < skills.length; ++i)
+	console.log(skills[i][0], skills[i][1], skills[i][2])
+
