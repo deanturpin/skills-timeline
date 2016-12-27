cacc5a9e99d17cded91cf29f92456c83ce6ac7ac
deanturpin <git@lvbtn.uk>
2016-12-27 00:48:00 +0000

    don't use full scale

--- skills.js
+++ skills.js
@@ -1,32 +1,38 @@
 "use strict"
 
+// Now
+const now = 2017
+
 // Skills and date range
 var skills = [
 
-	// Education
+	// "Skill",		start, end
+
+	// Education and employment
 	["University",	1994, 1998],
+	["Company1",	1998, now],
 
 	// Skills
-	["C++",			1996, 2017],
-	["Embedded",	1998, 2017],
-	["SC",			1998, 2017],
-	["Vim",			1998, 2017],
+	["C++",			1996, now],
+	["Embedded",	1998, now],
+	["SC",			1998, now],
+	["Vim",			1998, now],
 	["VxWorks",		1998, 2009],
 	["Unix",		1998, 2009],
-	["Networking",	2003, 2017],
-	["Regex",		2007, 2017],
-	["Bash",		2009, 2017],
-	["Linux	",		2009, 2017],
-	["Subversion",	2012, 2017],
-	["VMware",		2012, 2017],
-	["STL",			2013, 2017],
-	["C++11/14",	2015, 2017],
-	["Agile",		2015, 2017],
-	["Clang",		2015, 2017],
+	["Networking",	2003, now],
+	["Regex",		2007, now],
+	["Bash",		2009, now],
+	["Linux	",		2009, now],
+	["Subversion",	2012, now],
+	["VMware",		2012, now],
+	["STL",			2013, now],
+	["C++11/14",	2015, now],
+	["Agile",		2015, now],
+	["Clang",		2015, now],
 	["Iwyu",		2015, 2016],
-	["JavaScript",	2016, 2017],
-	["Git",			2015, 2017],
-	["Lua",			2016, 2017],
-	["Haskell",		2016, 2017],
+	["JavaScript",	2016, now],
+	["Git",			2015, now],
+	["Lua",			2016, now],
+	["Haskell",		2016, now],
 ]
 
